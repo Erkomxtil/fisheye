d305068446c0e63111f1d2fcf67502d5b57e4df3
Delastef <delastef@hotmail.comgit config --global user.name delastefegit config --global user.email delastef@hotmail.com>
2023-03-06 16:41:05 +0100

    Finalisation de la page d'accueil avec tous les éléments sur la page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,26 @@
 function photographerFactory(data) {
-	const { name, portrait } = data
+	const { name, id, city, country, tagline, price, portrait } = data
 
 	const picture = `assets/photographers/${portrait}`
 
 	function getUserCardDOM() {
-		const article = document.createElement( 'article' )
-		const img = document.createElement( 'img' )
-		img.setAttribute("src", picture)
-		const h2 = document.createElement( 'h2' )
-		h2.textContent = name
-		article.appendChild(img)
-		article.appendChild(h2)
+		const article = document.createElement( "article" )
+
+		article.innerHTML = `
+			<p>
+				<a href="" data-id=${id}>
+					<figure>
+						<img src="assets/photographers/${portrait}" alt=${name}>
+					</figure>
+					<h2>${name}</h2>
+				</a>
+			</p>
+			<h3>${city}, ${country}</h3>
+			<p>
+				${tagline}<br>
+				<span>${price}€/jour</span>
+			</p>
+		`
 		return (article)
 	}
 	return { name, picture, getUserCardDOM }
